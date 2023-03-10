9a3b8aa49822f0351104c57e98ea936712cd933a
Santiago Jimenez <santiagocj0212@gmail.com>
2023-03-10 02:27:05 -0500

    Commit country-DataContext

---
+++ Stores.Shared/Entities/Country.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stores.Shared.Entities
+{
+    public class Country
+    {
+
+        public int Id { get; set; }
+
+
+        [Display(Name = "Pais")]
+        [MaxLength(100, ErrorMessage = "Cuidado el campo{0} sólo puede contener {1} caracteres")]
+        [Required(ErrorMessage = "*El campo {0} es obligatorio ")]
+        public string Name { get; set; } = null;
+
+
+
+    }
+}
